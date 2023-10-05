24cda5d32369ff97462f31ceb10b8b86f2412a1d
Marco Camacho <marco1camacho1@gmail.com>
2023-10-04 21:29:57 -0400

    reinit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,77 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+      <div className={styles.topbar}>
+        <h1>Mindshard</h1>
+
+        <p>Turn any piece of data into a rich and insightful summary.</p>
+
+        <p>Demo</p>
+      </div>
+      <div className={styles.center}>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <p className={styles.center}>Input</p>{" "}
+          <div className={styles.container}>
+            <input type="text" placeholder="Enter text" className={styles.textInput} />
+            <p>or&nbsp;</p>
+            <input type="file" className={styles.fileInput} />
+          </div>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div>output</div>
       </div>
     </main>
-  )
+  );
 }
+// <main className={styles.main}>
+//   <div className={styles.description}>
+//     <p>
+//       Get started by editing&nbsp;
+//       <code className={styles.code}>src/app/page.tsx</code>
+//     </p>
+//     <div>
+//       <a href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
+//         By <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
+//       </a>
+//     </div>
+//   </div>
+
+//   <div className={styles.center}>
+//     <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
+//   </div>
+
+//   <div className={styles.grid}>
+//     <a href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" className={styles.card} target="_blank" rel="noopener noreferrer">
+//       <h2>
+//         Docs <span>-&gt;</span>
+//       </h2>
+//       <p>Find in-depth information about Next.js features and API.</p>
+//     </a>
+
+//     <a href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" className={styles.card} target="_blank" rel="noopener noreferrer">
+//       <h2>
+//         Learn <span>-&gt;</span>
+//       </h2>
+//       <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+//     </a>
+
+//     <a href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" className={styles.card} target="_blank" rel="noopener noreferrer">
+//       <h2>
+//         Templates <span>-&gt;</span>
+//       </h2>
+//       <p>Explore the Next.js 13 playground.</p>
+//     </a>
+
+//     <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" className={styles.card} target="_blank" rel="noopener noreferrer">
+//       <h2>
+//         Deploy <span>-&gt;</span>
+//       </h2>
+//       <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+//     </a>
+//   </div>
+// </main>;
---
+++ frontend/src/lib/components/misc/registry.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState } from "react";
+import { useServerInsertedHTML } from "next/navigation";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    return <>{styles}</>;
+  });
+
+  return <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>{children}</StyleSheetManager>;
+}
